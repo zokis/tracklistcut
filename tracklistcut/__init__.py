88349cb37e1e76bb31664670a46d8123f91d00ef
Pedro Valentim <pedro@znc.com.br>
2016-09-27 15:46:52 -0300

    fix conflicts

--- tracklistcut/__init__.py
+++ tracklistcut/__init__.py
@@ -3,7 +3,11 @@ import sys
 from pydub import AudioSegment
 
 
-def cut(file, tracklist):
+def get_human_time(time):
+    return str((time / (1000 * 60)) % 60) + ':' + str((time / 1000) % 60)
+
+
+def cut(file, tracklist, _regex='(\d:\d{2}:\d{2})[\s?](.*)'):
     sys.stdout.write('Starting tracklistcut...\n')
     sys.stdout.write('Getting sound from mp3 file with AudioSegment...')
     sound = AudioSegment.from_mp3(file)
@@ -12,7 +16,7 @@ def cut(file, tracklist):
     _names = []
     sys.stdout.write('Preparing times list...')
     for track in map(unicode.strip, map(unicode, tracklist)):
-        trackstart, trackname = re.findall('(\d:\d{2}:\d{2})[\s?](.*)', track)[0]
+        trackstart, trackname = re.findall(_regex, track)[0]
         _names.append(trackname)
         _trackstart = map(int, trackstart.split(':'))
         _trackstart[0] = _trackstart[0] * 60 * 60
@@ -23,17 +27,17 @@ def cut(file, tracklist):
     sys.stdout.write('...Done.\n')
     _times.append(len(sound))
     _names = iter(_names)
-    for n, time in enumerate(range(len(_times))):
+    for n, _ in enumerate(_times):
         if n == 0:
             continue
-        A1 = _times[time - 1]
-        A2 = _times[time]
+        A1 = _times[n - 1]
+        A2 = _times[n]
         trackname = next(_names)
         sys.stdout.write('\nCutting song #%s (%s.mp3) [<{%s - %s}>]\n' % (
             n,
             trackname,
-            str((A1 / (1000 * 60)) % 60) + ':' + str((A1 / 1000) % 60),
-            str((A2 / (1000 * 60)) % 60) + ':' + str((A2 / 1000) % 60)
+            get_human_time(A1),
+            get_human_time(A2)
         ))
         sound[A1:A2].export("%s.mp3" % trackname, format="mp3", bitrate="192k")
         sys.stdout.write('Song #%s (%s.mp3) saved.\n' % (n, trackname))
