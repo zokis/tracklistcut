18148ccae150d293304a0a181de73ea9be0e9d89
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2016-09-27 16:04:13 -0300

    separate into functions and add command line option

--- tracklistcut/__init__.py
+++ tracklistcut/__init__.py
@@ -12,16 +12,10 @@ def get_human_time(time):
     return str((time / (1000 * 60)) % 60).zfill(2) + ':' + str((time / 1000) % 60).zfill(2)
 
 
-def cut(file, tracklist, _regex='(\d:\d{2}:\d{2})[\s?](.*)', verbose=True):
-    if not isinstance(tracklist, (list, tuple)):
-        tracklist = tracklist.split('\n')
-    sysout('Starting tracklistcut...\n', verbose=verbose)
-    sysout('Getting sound from mp3 file with AudioSegment...', verbose=verbose)
-    sound = AudioSegment.from_mp3(file)
-    sysout('...Done.\n', verbose=verbose)
+def get_times_and_names(tracklist, sound, verbose, _regex):
+    sysout('Preparing times list...', verbose=verbose)
     _times = []
     _names = []
-    sysout('Preparing times list...', verbose=verbose)
     for track in map(unicode.strip, map(unicode, tracklist)):
         trackstart, trackname = re.findall(_regex, track)[0]
         _names.append(trackname)
@@ -31,6 +25,19 @@ def cut(file, tracklist, _regex='(\d:\d{2}:\d{2})[\s?](.*)', verbose=True):
         _trackstart[2] = _trackstart[2]
         trackstart_ms = sum(_trackstart) * 1000
         _times.append(trackstart_ms)
+    return _times, _names
+
+
+def cut(file, tracklist, _regex='(\d:\d{2}:\d{2})[\s?](.*)', verbose=True):
+    if not isinstance(tracklist, (list, tuple)):
+        tracklist = tracklist.split('\n')
+    sysout('Starting tracklistcut...\n', verbose=verbose)
+    sysout('Getting sound from mp3 file with AudioSegment...', verbose=verbose)
+    sound = AudioSegment.from_mp3(file)
+    sysout('...Done.\n', verbose=verbose)
+
+    _times, _names = get_times_and_names(tracklist, sound, verbose, _regex)
+
     sysout('...Done.\n', verbose=verbose)
     _times.append(len(sound))
     _names = iter(_names)
@@ -48,3 +55,7 @@ def cut(file, tracklist, _regex='(\d:\d{2}:\d{2})[\s?](.*)', verbose=True):
         ), verbose=verbose)
         sound[strat_time:end_time].export("%s.mp3" % trackname, format="mp3", bitrate="192k")
         sysout('Song #%s (%s.mp3) saved.\n' % (n, trackname), verbose=verbose)
+
+
+if __name__ == '__main__':
+    cut(sys.argv[1], open(sys.argv[2], 'w').readlines())
